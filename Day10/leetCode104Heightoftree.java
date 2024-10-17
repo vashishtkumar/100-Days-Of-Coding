23d693a33c07a35729e58baf406579f617993ecb
vashishtkumar <vashishtk270@gmail.com>
2024-10-17 22:15:59 +0530

    leetcode 104 maxdepthoftree

---
+++ Day10/leetCode104Heightoftree.java
@@ -0,0 +1,16 @@
+
+// leetcode 104
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+        return 0;
+    }
+
+    
+    int leftDepth = maxDepth(root.left);
+    int rightDepth = maxDepth(root.right);
+
+   
+    return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
