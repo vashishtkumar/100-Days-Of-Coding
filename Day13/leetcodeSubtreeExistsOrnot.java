85f3d526f4045b42a2dfee5e2af6b732e11cff0b
vashishtkumar <vashishtk270@gmail.com>
2024-11-07 18:53:47 +0530

    Day13 leetcode 572 subtreeExistsORnot

---
+++ Day13/leetcodeSubtreeExistsOrnot.java
@@ -0,0 +1,34 @@
+leetcode=572
+
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root==null)
+        return false;
+
+        if(root.val==subRoot.val){
+            if(isIdentical(root,subRoot))
+            return true;
+        }
+
+        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
+
+    }
+
+    public boolean isIdentical(TreeNode root,TreeNode subRoot){
+        if(root==null && subRoot==null){
+            return true;
+        }
+       else if(root==null || subRoot==null || root.val!=subRoot.val){
+        return false;
+       }
+       if(!isIdentical(root.left,subRoot.left)){
+        return false;
+       }
+       if(!isIdentical(root.right,subRoot.right)){
+        return false;
+       }
+       return true;
+    }
+}
+
+tc=O(n*m) sc=O(1);
