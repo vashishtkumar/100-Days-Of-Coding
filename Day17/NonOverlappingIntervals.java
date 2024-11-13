3ea197c6ca78526ac68c24267a7eec704d3f0583
vashishtkumar <vashishtk270@gmail.com>
2024-11-13 19:31:12 +0530

    Day 17 leetcode 435

---
+++ Day17/NonOverlappingIntervals.java
@@ -0,0 +1,19 @@
+public class NonOverlappingIntervals {
+    class Solution {
+        public int eraseOverlapIntervals(int[][] intervals) {
+           Arrays.sort(intervals,(a,b)->Integer.compare(a[1],b[1]));
+           int cnt=1;
+           int previndex=0;
+    
+           for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0] >= intervals[previndex][1]){
+                cnt++;
+                previndex=i;
+            }
+           }
+           return intervals.length-cnt;
+        }
+    }
+}
+
+tc=O(nlogn); sc=O(1);
